447d6d0048c25a3ebd5b71f49e5a1376eb71384c
PrabhatSinghPanwar <prabhatsingh348@gmail.com>
2021-05-28 19:12:55 +0530

    Adding AssTodo.js

---
+++ src/MyComponents/AddTodo.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from 'react';
+
+export default function AddTodo(props) {
+
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+
+    const submit = (e)=>{
+        e.preventDefault();
+        if(!title || !desc){
+            alert("Task or Description cannot be NULL...")
+        }
+        props.addTodo(title, desc) ;
+    }
+
+    return (
+        <div className="container my-3">
+            <h3>ADD A TASK</h3>
+            <form onSubmit={submit}>
+                <div className="mb-3">
+                    <label htmlFor="title" className="form-label">Task</label>
+                    <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} className="form-control" id="title" aria-describedby="emailHelp" placeholder="Work on Project"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="desc" className="form-label">Description</label>
+                    <input type="text" value={desc} onChange={(e)=>{setDesc(e.target.value)}} className="form-control" id="desc" placeholder="Complete the remaining project" />
+                </div>
+                        <button type="submit" className="btn btn-sm btn-success">ADD</button>
+            </form>
+        </div>
+    )
+}
