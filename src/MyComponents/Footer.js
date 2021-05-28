d4c754ac1e44d8edc5c1f18085cbb4db8f579e34
PrabhatSinghPanwar <prabhatsingh348@gmail.com>
2021-05-28 19:13:48 +0530

    Adding Footer.js

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Footer.css"
+
+export default function Footer() {
+    return (
+        <footer className=" text-light py-2">
+            <p className="text-center">Copyright &copy; MyToDosList.com </p>
+        </footer>
+    )
+}
