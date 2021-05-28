c71cb58e863f1e467223530ae3f67ece9de4e560
PrabhatSinghPanwar <prabhatsingh348@gmail.com>
2021-05-28 19:15:09 +0530

    Todos.js

---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import TodoItem from"./TodoItem";
+
+export default function Todos( props ) {                    //ans object props is pass you can retrive values by using props.item_name
+    return (
+        <div className="container">
+            
+            <h3 className=" my-3">Todos List</h3>
+            {/*{props.todos}*/}
+
+            { props.todos.length===0 ? "There is nothing to do in the list..." :
+            props.todos.map((todo)=>{
+                return ( <>
+                < TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete} />
+                <hr/>
+                </> );       //must send a unique key
+            })
+            } 
+            
+
+        </div>
+    )
+}
