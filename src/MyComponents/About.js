f89cd4729bca5980ac913ff7700a949491d8140e
PrabhatSinghPanwar <prabhatsingh348@gmail.com>
2021-05-29 13:06:02 +0530

    styling about section

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -2,15 +2,19 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div className="about">
-
-            this is about
-            <pre>
-                <p>
-                this is just a trial for about compenet so just sit and check weather it is 
-                so observe and this this component.
-                </p>
-            </pre>
-        </div>
+            <div className="container-sm pt-5">
+            <p className="mb-5 mt-5">
+                Created by Prabhat Singh Panwar.<br/><br/>
+                As a first project on react js framework. <br/>
+                In this app you can make a list of tasks that are import for you and you wand to keep a reminder those tasks 
+                and once you are done with the task you can delete the task just by clicking on the delete botton provided on the 
+                each task.<br/>
+                <br/>
+                This app will save a local file inside you local memory and add all the info in it so that on refresing the page
+                the element will still be there and not gets deleted.<br/>
+                This application is a react js framework appliction so there will be no reloding on clicking on the components of 
+                the application and due to which the expirence of the use.
+            </p>   
+            </div>
     )
 }
